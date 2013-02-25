b1f414e130d3ba9a0c00b6ccf66e61093a610781
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2013-02-25 15:55:59 +0100

    bugfix, fix parameter name

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,15 @@
 class elasticsearch::params {
 
   # No clustername set by default
-  $cluster_name      = ''
-  $node_name         = $::hostname
+  $cluster_name       = ''
+  $node_name          = $::hostname
 
-  $package           = 'elasticsearch'
-  $package_ensure    = 'installed'
-  $service           = 'elasticsearch'
-  $configdir         = '/etc/elasticsearch'
-  $configfile        = "${configdir}/elasticsearch.yml"
-  $configfile_source = template('elasticsearch/elasticsearch.yml.erb')
-  $service_name      = 'elasticsearch'
+  $package            = 'elasticsearch'
+  $package_ensure     = 'installed'
+  $service            = 'elasticsearch'
+  $configdir          = '/etc/elasticsearch'
+  $configfile         = "${configdir}/elasticsearch.yml"
+  $configfile_content = template('elasticsearch/elasticsearch.yml.erb')
+  $service_name       = 'elasticsearch'
 
 }
