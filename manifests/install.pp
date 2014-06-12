06c1422fa5def023416de2c5abe31052e89d2d8c
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2014-06-12 10:23:25 +0200

    depend on openjdk 7 headless

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,11 @@
 class elasticsearch::install {
 
   package {
+    'openjdk-7-jre-headless':
+      ensure => present;
     $elasticsearch::package:
-      ensure => $elasticsearch::package_ensure;
+      ensure  => $elasticsearch::package_ensure,
+      require => Package['openjdk-7-jre-headless'];
   }
 
 }
