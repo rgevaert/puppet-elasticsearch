3d3fa8c81151db5bac222dc8fae9456d2cd69afb
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2014-06-25 15:53:44 +0200

    script to backup elasticsearch

---
+++ manifests/backup.pp
@@ -0,0 +1,23 @@
+class elasticsearch::backup(  $instance,
+                              $backup_dir = '',
+                              $hour = '04',
+                              $min  = '0')
+{
+
+  cron {
+    'backup-elasticsearch':
+      ensure  => present,
+      command => "[ -x /usr/local/bin/es-backup ] && /usr/local/bin/es-backup ${instance} ${backupdir}",
+      hour    => $hour,
+      minute  => $minute;
+  }
+
+  file {
+    '/usr/local/bin/es-backup':
+      ensure  => present,
+      source  => 'puppet:///modules/elasticsearch/es-backup',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755';
+  }
+}
