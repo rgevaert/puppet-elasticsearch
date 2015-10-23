9a622e88559bdf568f9f8b3a2acb3bc24f2b5fee
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2015-10-23 17:03:38 +0200

    prevent duplicate declaration conficts

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,8 @@
 class elasticsearch::install {
 
+  ensure_packages('openjdk-7-jre-headless')
+
   package {
-    'openjdk-7-jre-headless':
-      ensure => present;
     $elasticsearch::package:
       ensure  => $elasticsearch::package_ensure,
       require => Package['openjdk-7-jre-headless'];
