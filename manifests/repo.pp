cd100f0b9bdb8a6ad420c10cfdd16a516b795466
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2015-11-24 17:34:24 +0100

    fix repo url

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -2,12 +2,12 @@ class elasticsearch::repo {
 
   apt::key {
     'elasticsearch.org':
-      source => 'http://packages.elasticsearch.org/GPG-KEY-elasticsearch';
+      source => 'http://packages.elastic.co/GPG-KEY-elasticsearch';
   }
 
   $repo = $::operatingsystem ? {
-        'Debian' => "deb http://packages.elasticsearch.org/elasticsearch/${::elasticsearch::major_release}/debian stable main",
-        'Ubuntu' => "deb http://packages.elasticsearch.org/elasticsearch/${::elasticsearch::major_release}/debian stable main",
+        'Debian' => "deb http://packages.elastic.co/elasticsearch/${::elasticsearch::major_release}/debian stable main",
+        'Ubuntu' => "deb http://packages.elastic.co/elasticsearch/${::elasticsearch::major_release}/debian stable main",
         default  => fail("${::operatingsystem} not supported in elasticsearch::repo")
   }
 
