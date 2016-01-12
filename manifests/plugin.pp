5163fea6e843ddde91d99cb2e8aa1f8739a9e399
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2016-01-12 15:33:40 +0100

    when already installed do not try to install again

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,6 +1,12 @@
-define elasticsearch::plugin {
+# example
+#  elasticsearch::plugin {
+#    'kopf':
+#      install_name => 'lmenezes/elasticsearch-kopf/2.0';
+#  }
+define elasticsearch::plugin ($install_name) {
   exec {
     $name:
-      command  => "/usr/share/elasticsearch/bin/plugin install ${name}";
+      command  => "/usr/share/elasticsearch/bin/plugin install ${install_name}",
+      creates  => "/usr/share/elasticsearch/plugins/${name}";
   }
 }
