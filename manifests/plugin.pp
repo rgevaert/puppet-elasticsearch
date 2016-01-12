6d5ba9c8078a1396a4f8315677dd8d36546f2cb4
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2016-01-12 15:23:17 +0100

    fix Error: Could not find command elasticsearch/bin/plugin

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,7 +1,8 @@
 define elasticsearch::plugin {
   exec {
     $name:
-      cwd     => '/usr/share',
-      command => "elasticsearch/bin/plugin install install ${name}";
+      provider => 'shell',
+      cwd      => '/usr/share',
+      command  => "elasticsearch/bin/plugin install install ${name}";
   }
 }
